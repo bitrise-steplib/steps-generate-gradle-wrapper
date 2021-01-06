affcdcd771a0d1ffa8fc0231df2604cd3168ad77
Imre Kelényi <imre.kelenyi@gmail.com>
2021-01-06 18:04:46 +0100

    add envman

--- step.sh
+++ step.sh
@@ -5,6 +5,8 @@ set -e
 cd $project_root_dir
 
 gradle wrapper --gradle-version $gradle_version
-export GRADLEW_PATH="$(realpath gradlew)"
+
+GRADLEW_PATH="$(realpath gradlew)"
+envman add --key GRADLEW_PATH --value "$GRADLEW_PATH"
 
 echo "Gradle wrapper is available at $GRADLEW_PATH"
