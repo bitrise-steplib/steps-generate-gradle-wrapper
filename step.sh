ffe8a62d2bdfaf7af687297396d605acbc53bae8
Imre Kelényi <imre.kelenyi@gmail.com>
2021-01-06 17:50:40 +0100

    cleanup

--- step.sh
+++ step.sh
@@ -2,25 +2,9 @@
 
 set -e
 
-# THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# tmp_gopath_dir="$(mktemp -d)"
-
-# go_package_name="github.com/bitrise-steplib/steps-generate-gradle-wrapper"
-# full_package_path="${tmp_gopath_dir}/src/${go_package_name}"
-# mkdir -p "${full_package_path}"
-
-# rsync -avh --quiet "${THIS_SCRIPT_DIR}/" "${full_package_path}/"
-
-# export GOPATH="${tmp_gopath_dir}"
-# export GO15VENDOREXPERIMENT=1
-# go run "${full_package_path}/main.go"
-
-pushd $project_root_dir
+cd $project_root_dir
 
 gradle wrapper --gradle-version $gradle_version
 export GRADLEW_PATH="$(realpath gradlew)"
 
 echo "Gradle wrapper is available at $GRADLEW_PATH"
-
-popd
