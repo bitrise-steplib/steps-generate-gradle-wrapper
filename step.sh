414bb69fa2fcf647eb8b6c46d6975fc2e69a3628
Imre Kelényi <imre.kelenyi@gmail.com>
2021-01-06 17:13:56 +0100

    test gralew generation

--- step.sh
+++ step.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 set -ex
-THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-tmp_gopath_dir="$(mktemp -d)"
+# THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-go_package_name="github.com/bitrise-steplib/steps-generate-gradle-wrapper"
-full_package_path="${tmp_gopath_dir}/src/${go_package_name}"
-mkdir -p "${full_package_path}"
+# tmp_gopath_dir="$(mktemp -d)"
 
-rsync -avh --quiet "${THIS_SCRIPT_DIR}/" "${full_package_path}/"
+# go_package_name="github.com/bitrise-steplib/steps-generate-gradle-wrapper"
+# full_package_path="${tmp_gopath_dir}/src/${go_package_name}"
+# mkdir -p "${full_package_path}"
 
-export GOPATH="${tmp_gopath_dir}"
-export GO15VENDOREXPERIMENT=1
-go run "${full_package_path}/main.go"
+# rsync -avh --quiet "${THIS_SCRIPT_DIR}/" "${full_package_path}/"
+
+# export GOPATH="${tmp_gopath_dir}"
+# export GO15VENDOREXPERIMENT=1
+# go run "${full_package_path}/main.go"
+
+pushd $project_root_dir
+
+gradle wrapper --gradle-version $gradle_version
+export GRADLEW_PATH="$(realpath gradlew)"
+
+popd
